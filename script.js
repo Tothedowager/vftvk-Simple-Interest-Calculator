f159628bb12ef063cb40092a04926c756b286ae0
AlyxIs <45057376+Tothedowager@users.noreply.github.com>
2021-03-04 08:31:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function compute()
     principal = Number(document.getElementById("principal").value);
     years = Number(document.getElementById("years").value);
     // write logic to determine interest from user input 
-    interest = principal * rate * years/100    
+    interest = principal * rate * years/100;    
     // get current year and calculate return year from user input  
     date = new Date().getFullYear();
     year = date + years;
@@ -15,18 +15,18 @@ function compute()
     // if statement to determine if principal value entered is a positive integer 
     if (principal <=0) {
         alert('Enter a positive number')
-        document.getElementById('principal').focus()
+        document.getElementById('principal').focus();
     }else {
     // return message broken into segments to accomodate highlighted values
     result.innerHTML = 'If you deposit ' + `<i>${principal}</i>` + ',<br>'
                      + 'at an interest rate of ' + `<i>${rate}%</i>` + '.<br>'
                      + 'You will receive an amount of ' + `<i>${interest}</i>`+ ',<br>'
-                     + 'in the year ' + `<i>${year}</i>`
+                     + 'in the year ' + `<i>${year}</i>`;
     }
 }
 // function to update rangeslider value 
 function returnRate(){
     rate = Number(document.getElementById("rate").value);
     result = document.getElementById('return');
-    result.innerHTML= rate + '%
+    result.innerHTML= rate + '%;
 }
